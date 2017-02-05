42954436523697c62eb8a5bb11a8b42947bc75e4
sjindal995 <sjindal995@gmail.com>
2017-02-05 00:32:47 -0800

    opencv installed

--- ass1/a1.cpp
+++ ass1/a1.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/utility.hpp>
 
 using namespace std;
+using namespace cv;
 
 class vector{
 	double x;
