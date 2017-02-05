c4a51134c1fe833c44fa78c338961ca16a9aa261
sjindal995 <sjindal995@gmail.com>
2017-02-05 22:05:56 +0530

    fixes in new.cpp

--- ass1/a1.cpp
+++ ass1/a1.cpp
@@ -1,34 +1,5 @@
-#include <iostream>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/core/utility.hpp>
+#include "new.cpp"
 
-using namespace std;
-using namespace cv;
+int main(){
 
-class vector{
-	double x;
-	double y;
-	double z;
-	double dotProd(vector v2);
-	vector crossProd(vector v2);
-	double getMag();
-};
-
-vector::vector(double _x, double _y, double _z){
-	x = _x;
-	y = _y;
-	z = _z;
-}
-
-double vector::dotProd(vector v2){
-	return (x*v2.x + y*v2.y + z*v2.z);
-}
-
-vector vector::crossProd(vector v2){
-	vector v3 = new vector();
-	v3.x = y*v2.z - z*v2.y;
-	v3.y = z*v2.x - x*v2.z;
-	v3.z = x*v2.y - y*v2.x;
-	return v3;
 }
--- ass1/new.cpp
+++ ass1/new.cpp
@@ -8,24 +8,35 @@
 using namespace cv; // all the new API is put into "cv" namespace. Export its content
 using namespace std;
 
-class Vec{
+class nVec{
+public:
 	double x;
 	double y;
 	double z;
+	nVec();
+	nVec(double _x,double _y, double _z);
+	double dotProd(nVec v2);
+	nVec crossProd(nVec v2);
+};
+
+nVec::nVec(){
+	x=0;
+	y=0;
+	z=0;
 }
 
-Vec::Vec(double _x, double _y, double _z){
+nVec::nVec(double _x, double _y, double _z){
 	x = _x;
 	y = _y;
 	z = _z;
 }
 
-double Vec::dotProd(Vec v2){
+double nVec::dotProd(nVec v2){
 	return (x*v2.x + y*v2.y + z*v2.z);
 }
 
-Vec Vec::crossProd(Vec v2){
-	Vec v3 = new Vec();
+nVec nVec::crossProd(nVec v2){
+	nVec v3;
 	v3.x = y*v2.z - z*v2.y;
 	v3.y = z*v2.x - x*v2.z;
 	v3.z = x*v2.y - y*v2.x;
@@ -34,39 +45,60 @@ Vec Vec::crossProd(Vec v2){
 
 class Ray
 {
-	Vec r0;
-	Vec rd;
+public:
+	nVec r0;
+	nVec rd;
+	Ray(nVec _r0, nVec _rd);
+};
 
-}
-
-Ray::Ray(Vec _r0,Vec _rd ){
+Ray::Ray(nVec _r0,nVec _rd ){
 	r0 = _r0;
 	rd = _rd;
 }
 
 class Sphere
 {
-	Vec c;
+public:
+	nVec c;
 	int r;
-}
+	Sphere(nVec _c, int _r);
+};
 
-Sphere::Sphere(Vec _c,int _r ){
+Sphere::Sphere(nVec _c,int _r ){
 	c = _c;
 	r = _r;
 }
 
 class Polygon
 {
+public:
 	int n;
-	vector<Vec> vertices;
+	vector<nVec> vertices;
+	Polygon(int _n, vector<nVec> _vertices);
+};
+
+Polygon::Polygon(int _n,vector<nVec> _vertices){
+	n = _n;
+	vertices = _vertices;
 }
 
-Polygon::Polygon(int _c,vector<Vec> ){
-	c = _c;
-	r = _r;
+class Screen{
+public:
+	int l;
+	int b;
+	int center;
+	nVec normal;
+	Screen(int _l, int _b, int _center, nVec _normal);
 }
 
-int main{
-
-
+Screen::Screen(int _l, int _b, int _center, nVec _normal){
+	l = _l;
+	b = _b;
+	center = _center;
+	normal = _normal;
 }
+
+// int main(){
+
+
+// }
