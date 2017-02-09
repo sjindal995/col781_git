3e0d7c97e840fdd75e8bb11ebf8ced08eef9877b
sjindal995 <sjindal995@gmail.com>
2017-02-09 21:30:46 +0530

    submission 1 done and edited in folder 2013CS10245_2013CS10253

---
+++ ass1/2013CS10245_2013CS10253/a1.cpp
@@ -0,0 +1,459 @@
+#include "new.cpp"
+#include <fstream>
+vector<Sphere> spheres;
+
+vector<Polygon> polygons;
+
+vector<nVec> lights_srcs;
+vector<double> i_l;
+double i_a;
+int image_length,image_height;
+double pixel_d;
+int alias_factor = 3;
+
+Vec3b traceRay(Ray r, double factor){
+	if(factor < 0.001) return 0;
+	double t=1000000;
+	bool pol_intersect = false;
+	bool sphere_intersect = false;
+	int int_k = -1, aff=0;
+	Mat transm;
+
+	for(int k=0; k<spheres.size(); k++){
+		// if(sphere.affine==0){
+		pair<double,double> int1 = spheres[k].intersect(r);
+		// }
+		// else{
+		// 	pair<double,double> int1 = spheres[k].intersect()
+		// }
+		if(int1.first !=-1){
+			t = min(min(int1.first,int1.second), t);
+			if(t == min(int1.first, int1.second)){
+				// img.at<Vec3b>(i+s.l/2,j+s.b/2) = spheres[k].color;
+				if(spheres[k].affine==1) {
+					aff=1;
+					transm = spheres[k].m;
+				}
+				else aff=0;
+				sphere_intersect = true;
+				int_k = k;
+			}
+
+		}
+	}
+	for(int k=0;k<polygons.size(); k++){
+		double int2 = polygons[k].intersect(r);
+		if(int2 !=-1){
+			t = min(t,int2);
+			if(t == int2){
+				// img.at<Vec3b>(i+s.l/2,j+s.b/2) = polygons[k].color;
+				if(polygons[k].affine==1) {
+					aff=1;
+					transm = polygons[k].m;
+				}
+				else aff=0;
+				pol_intersect = true;
+				int_k = k;
+			} 
+		}
+	}
+	Vec3b color = Vec3b(0,0,0);
+	double i_total = 0;
+	if (sphere_intersect || pol_intersect){
+		if(pol_intersect){
+			i_total += polygons[int_k].ka*i_a;
+		}
+		else{
+			i_total += spheres[int_k].ka*i_a;
+		}
+
+		nVec pt = r.Point(t);
+		if(aff==1){
+			pt = (r.r0.transform(transm.inv(),0) + r.rd.transform(transm.inv(),1).scale(t) ).transform(transm,0);	
+		}
+		nVec N;
+		if(pol_intersect){
+			N = polygons[int_k].normal().scale(-1);
+		}
+		else{
+			N = spheres[int_k].normal(pt);
+		}
+		for(int ll=0; ll<lights_srcs.size(); ll++){
+			
+			nVec l_src = lights_srcs[ll];
+			// l_src.print();
+			nVec L = l_src - pt;
+			L = L.norm();
+			nVec R = N.scale(2*L.dotProd(N)) - L;
+			nVec V = r.r0 - pt;
+			V = V.norm();
+			R = R.norm();
+			bool blocked = false;
+			Ray light_ray(pt, L);
+			for(int k1=0;k1<spheres.size();k1++){
+				
+				pair<double,double> pr = spheres[k1].intersect(light_ray);
+				if(pr.first > pow(10,-3)) {
+					blocked = true;
+					break;
+				}
+			}
+			if(!blocked){
+				for(int k1=0;k1<polygons.size();k1++){
+					double pr = polygons[k1].intersect(light_ray);
+					if(pr > pow(10,-3)) {
+						blocked = true;
+						break;
+					}
+				}
+			}
+			if(!blocked ){
+				if(pol_intersect){
+					i_total += max(polygons[int_k].kd*i_l[ll]*(L.dotProd(N)),0.0) + polygons[int_k].ks*i_l[ll]*pow(max(0.0,R.dotProd(V)),polygons[int_k].spec_coeff);
+				}
+				else{
+					i_total += max(spheres[int_k].kd*i_l[ll]*(L.dotProd(N)),0.0) + spheres[int_k].ks*i_l[ll]*pow(max(0.0,R.dotProd(V)),spheres[int_k].spec_coeff);
+				}
+			}
+			
+		}
+		if(pol_intersect){
+			nVec ref = N.scale(2*(r.rd).dotProd(N)) - r.rd;
+			Ray reflected_ray(pt, ref);
+			Vec3b newColor = traceRay(reflected_ray,factor*polygons[int_k].reflection);
+			color[0] = min(newColor[0] + factor*polygons[int_k].color[0]*i_total,255.0);
+			color[1] = min(newColor[1] + factor*polygons[int_k].color[1]*i_total,255.0);
+			color[2] = min(newColor[2] + factor*polygons[int_k].color[2]*i_total,255.0);
+		}
+		else{
+			nVec ref = N.scale(2*(r.rd).dotProd(N)) - r.rd;
+			Ray reflected_ray(pt, ref);
+			Vec3b newColor = traceRay(reflected_ray,factor*spheres[int_k].reflection);
+			color[0] = min(newColor[0] + factor*spheres[int_k].color[0]*i_total,255.0);
+			color[1] = min(newColor[1] + factor*spheres[int_k].color[1]*i_total,255.0);
+			color[2] = min(newColor[2] + factor*spheres[int_k].color[2]*i_total,255.0);
+		}
+
+	}
+	return color;
+}
+
+Mat illuminateModel(nVec camera, Screen s){
+	Mat tmp_img(image_height,image_length, CV_8UC3, Vec3b(0,0,0));
+	// nVec l_src;
+	double i=0,j=0;
+	for(double it1 = 0; it1 < image_height; it1++){
+		for(double it2 = 0; it2 < image_length; it2++){
+			i = (it1 - image_height/2.0)*pixel_d;
+			j = (it2 - image_length/2.0)*pixel_d;
+			nVec pixel = s.center + (s.up.scale(i)) + (s.
+					right.scale(j));
+			nVec rd = pixel - camera;
+			rd = rd.norm();
+			Ray r(camera, rd);
+			// double t=1000000;
+			// bool pol_intersect = false;
+			// bool sphere_intersect = false;
+			// int int_k = -1, aff=0;
+			// Mat transm;
+
+			// for(int k=0; k<spheres.size(); k++){
+			// 	// if(sphere.affine==0){
+			// 	pair<double,double> int1 = spheres[k].intersect(r);
+			// 	// }
+			// 	// else{
+			// 	// 	pair<double,double> int1 = spheres[k].intersect()
+			// 	// }
+			// 	if(int1.first !=-1){
+			// 		t = min(min(int1.first,int1.second), t);
+			// 		if(t == min(int1.first, int1.second)){
+			// 			// img.at<Vec3b>(i+s.l/2,j+s.b/2) = spheres[k].color;
+			// 			if(spheres[k].affine==1) {
+			// 				aff=1;
+			// 				transm = spheres[k].m;
+			// 			}
+			// 			else aff=0;
+			// 			sphere_intersect = true;
+			// 			int_k = k;
+			// 		}
+
+			// 	}
+			// }
+			// for(int k=0;k<polygons.size(); k++){
+			// 	double int2 = polygons[k].intersect(r);
+			// 	if(int2 !=-1){
+			// 		t = min(t,int2);
+			// 		if(t == int2){
+			// 			// img.at<Vec3b>(i+s.l/2,j+s.b/2) = polygons[k].color;
+			// 			if(polygons[k].affine==1) {
+			// 				aff=1;
+			// 				transm = polygons[k].m;
+			// 			}
+			// 			else aff=0;
+			// 			pol_intersect = true;
+			// 			int_k = k;
+			// 		} 
+			// 	}
+			// }
+			// // if(!sphere_intersect && !pol_intersect) continue;
+
+			// // nVec pt = camera + rd.scale(t);
+
+			// double i_total = 0;
+			// if (sphere_intersect || pol_intersect){
+			// 	if(pol_intersect){
+			// 		i_total += polygons[int_k].ka*i_a;
+			// 	}
+			// 	else{
+			// 		i_total += spheres[int_k].ka*i_a;
+			// 	}
+
+			// 	for(int ll=0; ll<lights_srcs.size(); ll++){
+					
+			// 		nVec l_src = lights_srcs[ll];
+			// 		// l_src.print();
+			// 		nVec pt = r.Point(t);
+			// 		if(aff==1){
+			// 			pt = (r.r0.transform(transm.inv(),0) + r.rd.transform(transm.inv(),1).scale(t) ).transform(transm,0);	
+			// 		}
+			// 		nVec L = l_src - pt;
+			// 		L = L.norm();
+			// 		nVec N;
+			// 		if(pol_intersect){
+			// 			N = polygons[int_k].normal().scale(-1);
+			// 			// N.print();
+			// 		}
+			// 		else{
+			// 			// N = pt - spheres[int_k].c;
+			// 			// N = N.norm();
+			// 			N = spheres[int_k].normal(pt);
+			// 		}
+			// 		nVec R = N.scale(2*L.dotProd(N)) - L;
+			// 		nVec V = camera - pt;
+			// 		V = V.norm();
+			// 		R = R.norm();
+			// 		bool blocked = false;
+			// 		Ray light_ray(pt, L);
+			// 		for(int k1=0;k1<spheres.size();k1++){
+			// 			// if(k1 != int_k){
+
+			// 			pair<double,double> pr = spheres[k1].intersect(light_ray);
+			// 			// if(pr.first!=-1) cout<<"dsds"<<endl;
+			// 			// cout<<pr.first<<" "<<pr.second<<endl;
+			// 			if(pr.first > pow(10,-3)) {
+			// 				// cout<<"here"<<endl;
+			// 				blocked = true;
+			// 				break;
+			// 			}
+			// 			// }
+			// 		}
+			// 		if(!blocked){
+			// 			for(int k1=0;k1<polygons.size();k1++){
+			// 				double pr = polygons[k1].intersect(light_ray);
+			// 				if(pr > pow(10,-3)) {
+			// 					blocked = true;
+			// 					break;
+			// 				}
+			// 			}
+			// 		}
+			// 		if(!blocked ){
+			// 			if(pol_intersect){
+			// 				i_total += max(polygons[int_k].kd*i_l[ll]*(L.dotProd(N)),0.0) + polygons[int_k].ks*i_l[ll]*pow(max(0.0,R.dotProd(V)),polygons[int_k].spec_coeff);
+			// 				// cout << "1: " << (L.dotProd(N)) << ", 2: " << R.dotProd(V) << endl;
+			// 			}
+			// 			else{
+			// 				i_total += max(spheres[int_k].kd*i_l[ll]*(L.dotProd(N)),0.0) + spheres[int_k].ks*i_l[ll]*pow(max(0.0,R.dotProd(V)),spheres[int_k].spec_coeff);
+			// 			}
+			// 		}
+
+			// 	}
+
+			// 	if(pol_intersect){
+			// 		// cout << img.at<Vec3b>(i+s.l/2,j+s.b/2) <<  " , "  << blocked << ", pixel: " << pixel.x << "," << pixel.y << ","<<pixel.z << endl;
+			// 		// for(int i1 = 0;i1 < pixel_d;i1++){
+			// 		// 	for(int j1=0;j1 < pixel_d;j1++){
+			// 				tmp_img.at<Vec3b>(it1,it2)[0] = min(polygons[int_k].color[0]*i_total,255.0);
+			// 				tmp_img.at<Vec3b>(it1,it2)[1] = min(polygons[int_k].color[1]*i_total,255.0);
+			// 				tmp_img.at<Vec3b>(it1,it2)[2] = min(polygons[int_k].color[2]*i_total,255.0);
+			// 		// 	}
+			// 		// }
+			// 	}
+			// 	else{
+			// 		// for(int i1 = 0;i1 < pixel_d;i1++){
+			// 		// 	for(int j1=0;j1 < pixel_d;j1++){
+			// 				tmp_img.at<Vec3b>(it1,it2)[0] = min(spheres[int_k].color[0]*i_total,255.0);
+			// 				tmp_img.at<Vec3b>(it1,it2)[1] = min(spheres[int_k].color[1]*i_total,255.0);
+			// 				tmp_img.at<Vec3b>(it1,it2)[2] = min(spheres[int_k].color[2]*i_total,255.0);
+			// 		// 	}
+			// 		// }
+			// 	}
+			tmp_img.at<Vec3b>(it1,it2) = traceRay(r,1);
+				
+			// }
+
+		}
+	}
+	return tmp_img;
+	// GaussianBlur(img,img,Size(5,5),0.5);
+	// imshow("image", img);
+	// waitKey(0);
+}
+
+void antiAlias(nVec camera, Screen s){
+	Mat img(image_height, image_length, CV_8UC3, Vec3b(0,0,0));
+	image_height = alias_factor*image_height;
+	image_length = alias_factor*image_length;
+	pixel_d = pixel_d/float(alias_factor);
+	Mat tmp_img = illuminateModel(camera,s);
+	image_height = image_height/alias_factor;
+	image_length = image_length/alias_factor;
+	for(int i=0;i<image_height;i++){
+		for(int j=0;j<image_length;j++){
+			double x_c=0,y_c=0,z_c=0;
+			for(int i0=0;i0<alias_factor;i0++){
+				for(int j0=0;j0<alias_factor;j0++){
+					x_c += tmp_img.at<Vec3b>(i*alias_factor + i0, j*alias_factor + j0)[0];
+					y_c += tmp_img.at<Vec3b>(i*alias_factor + i0, j*alias_factor + j0)[1];
+					z_c += tmp_img.at<Vec3b>(i*alias_factor + i0, j*alias_factor + j0)[2];
+				}
+			}
+			img.at<Vec3b>(i,j) = Vec3b(x_c/(alias_factor*alias_factor), y_c/(alias_factor*alias_factor), z_c/(alias_factor*alias_factor));
+		}
+	}
+	imshow("image", img);
+	waitKey(0);
+}
+
+
+// void generateImage(nVec camera, Screen s){
+// 	Mat img(s.l, s.b, CV_8UC3,Vec3b(0,0,0));
+// 	for(int i=-1*s.l/2; i <= s.l/2;i++){
+// 		for(int j = -1*s.b/2; j <= s.b/2;j++){
+// 			//traverse over all objects, and take the first	
+// 			nVec pixel = s.center + (s.up.scale(i)) + (s.
+// 					right.scale(j));
+// 			nVec rd = pixel - camera;
+// 			rd = rd.norm();
+// 			Ray r(camera, rd);
+// 			double t=1000000;
+
+// 			for(int k=0; k<spheres.size(); k++){
+// 				pair<double,double> int1 = spheres[k].intersect(r);
+// 				if(int1.first !=-1){
+// 					t = min(min(int1.first,int1.second), t);
+// 					if(t == min(int1.first, int1.second)){
+// 						img.at<Vec3b>(i+s.l/2,j+s.b/2) = spheres[k].color;
+						
+// 					}
+
+// 				}
+// 			}
+// 			for(int k=0;k<polygons.size(); k++){
+// 				double int2 = polygons[k].intersect(r);
+// 				if(int2 !=-1){
+// 					t = min(t,int2);
+// 					if(t == int2){
+// 						// cout << t << endl;
+// 						img.at<Vec3b>(i+s.l/2,j+s.b/2) = polygons[k].color;
+
+// 					} 
+// 				}
+// 			}
+// 		}
+// 	}
+// 	imshow("img", img);
+// 	waitKey(0);
+	
+// }
+
+int main(int argc, char** argv){
+	ifstream inp(argv[1]);
+	// ifstream inp("input.txt");
+	string tag;
+	inp >> tag;
+	double x,y,z;
+	inp>>x>>y>>z;
+	nVec camera(x,y,z);
+
+	inp>>tag>>x>>y>>z;
+	nVec sc_center(x,y,z);
+	inp>>tag>>x>>y>>z;
+	nVec sc_normal(x,y,z);
+	inp>>tag>>x>>y>>z;
+	nVec sc_up(x,y,z);
+	inp>>tag>>x>>y>>z;
+	nVec sc_right(x,y,z);
+	inp>>tag>>pixel_d;
+	inp>>tag>>image_length>>image_height;
+
+	Screen sc(sc_center,sc_normal,sc_up,sc_right);
+
+	int n_spheres;
+	inp>>tag>>n_spheres;
+	for(int i=0;i<n_spheres;i++){
+		inp>>tag>>x>>y>>z;
+		nVec sp_center(x,y,z);
+		double sp_radius;
+		inp>>tag>>sp_radius;
+		inp>>tag>>x>>y>>z;
+		Vec3b sp_color = Vec3b(0,0,255);
+		double ka,kd,ks,spec_coeff;
+		inp>>tag>>ka>>kd>>ks>>spec_coeff;
+		Sphere sp(sp_center,sp_radius,sp_color,ka,kd,ks,spec_coeff);
+		spheres.push_back(sp);
+	}
+
+
+	int n_pol;
+	inp>>tag>>n_pol;
+	for(int i=0;i<n_pol;i++){
+		int n_vertices;
+		inp>>tag>>n_vertices;
+		vector<nVec> vertices;
+		for(int j=0;j<n_vertices;j++){
+			inp>>tag>>x>>y>>z;
+			vertices.push_back(nVec(x,y,z));
+		}
+		inp>>tag>>x>>y>>z;
+		Vec3b p_color = Vec3b(x,y,z);
+		double ka,kd,ks,spec_coeff;
+		inp>>tag>>ka>>kd>>ks>>spec_coeff;
+		Polygon p(n_vertices,vertices,p_color,ka,kd,ks,spec_coeff);
+		polygons.push_back(p);
+	}
+
+	// polygons[0].affine=1;
+	// Mat mat(4,4, CV_32FC1, float(0));
+	// mat.at<float>(0,0)=1;
+	// mat.at<float>(1,0)=-1;
+	// mat.at<float>(1,1)=2;
+	// mat.at<float>(2,2)=3;
+	// mat.at<float>(3,3)=1;
+	// polygons[0].m = mat;
+
+	// spheres[0].affine=1;
+	// Mat mat2(4,4, CV_32FC1, float(0));
+	// mat2.at<float>(0,0)=0.5;
+	// mat2.at<float>(1,0)=0.2;
+	// mat2.at<float>(1,1)=0.5;
+	// mat2.at<float>(2,2)=0.5;
+	// // mat2.at<float>(2,0)=1;
+	// mat2.at<float>(3,3)=1;
+	// spheres[0].m = mat2;
+
+	int n_lights;
+	inp>>tag>>n_lights;
+	for(int i=0;i<n_lights;i++){
+		inp>>tag>>x>>y>>z;
+		lights_srcs.push_back(nVec(x,y,z));
+		double l;
+		inp>>tag>>l;
+		i_l.push_back(l);
+	}
+	inp>>tag>>i_a;
+	inp>>tag>>image_length>>image_height;
+	// illuminateModel(camera,sc);
+
+	// Mat matrix(4,4,CV_32FC1)
+	antiAlias(camera,sc);
+}
---
+++ ass1/2013CS10245_2013CS10253/new.cpp
@@ -0,0 +1,370 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/utility.hpp>
+#include <math.h>
+#include <algorithm>
+
+using namespace cv; // all the new API is put into "cv" namespace. Export its content
+using namespace std;
+
+class nVec{
+public:
+	double x;
+	double y;
+	double z;
+	nVec(double _x = 0,double _y = 0, double _z = 0);
+	double dotProd(nVec v2);
+	nVec crossProd(nVec v2);
+	nVec operator+(const nVec& nv2);
+	nVec operator-(const nVec& nv2);
+	nVec norm();
+	nVec scale(double d);
+	nVec transform(Mat, int);
+	void print();
+
+	// nVec sub(nVec v2);
+	// nVec add(nVec v2);
+	// nVec multiply(double d);
+	// nVec divide(double d);
+	double getMag();
+};
+
+nVec::nVec(double _x, double _y, double _z){
+	x = _x;
+	y = _y;
+	z = _z;
+}
+
+nVec nVec::scale(double d){
+	nVec v;
+	v.x = x*d;
+	v.y = y*d;
+	v.z = z*d;
+	return v;
+}
+
+nVec nVec::norm(){
+	double d = sqrt(x*x+y*y+z*z);
+	return scale(1/d);
+}
+
+void nVec::print(){
+	cout<<x<<" "<<y<<" "<<z<<endl;
+}
+
+
+nVec nVec::operator+(const nVec& nv) {
+
+	nVec nv2;
+	nv2.x = this->x+nv.x;
+	nv2.y = this->y+nv.y;
+	nv2.z = this->z+nv.z;
+
+	return nv2;
+}
+
+nVec nVec::operator-(const nVec& nv) {
+
+	nVec nv2;
+	nv2.x = this->x-nv.x;
+	nv2.y = this->y-nv.y;
+	nv2.z = this->z-nv.z;
+
+	return nv2;
+}
+
+
+double nVec::dotProd(nVec v2){
+	return (x*v2.x + y*v2.y + z*v2.z);
+}
+
+nVec nVec::crossProd(nVec v2){
+	nVec v3;
+	v3.x = y*v2.z - z*v2.y;
+	v3.y = z*v2.x - x*v2.z;
+	v3.z = x*v2.y - y*v2.x;
+	return v3;
+}
+
+double nVec::getMag(){
+	return sqrt(x*x + y*y + z*z);
+}
+
+nVec nVec::transform(Mat mat, int type){
+	nVec v;
+	if(type==1){
+		//ray direction
+		v.x = mat.at<float>(0,0)*x + mat.at<float>(1,0)*y + mat.at<float>(2,0)*z; 
+		v.y = mat.at<float>(0,1)*x + mat.at<float>(1,1)*y + mat.at<float>(2,1)*z; 
+		v.z = mat.at<float>(0,2)*x + mat.at<float>(1,2)*y + mat.at<float>(2,2)*z;
+	}
+	else{
+		v.x = mat.at<float>(0,0)*x + mat.at<float>(1,0)*y + mat.at<float>(2,0)*z + mat.at<float>(3,0); 
+		v.y = mat.at<float>(0,1)*x + mat.at<float>(1,1)*y + mat.at<float>(2,1)*z + mat.at<float>(3,1); 
+		v.z = mat.at<float>(0,2)*x + mat.at<float>(1,2)*y + mat.at<float>(2,2)*z + mat.at<float>(3,2);
+
+	}
+	return v; 
+}
+
+class Ray
+{
+public:
+	nVec r0;
+	nVec rd;
+	Ray();
+	Ray(nVec _r0, nVec _rd);
+	nVec Point(double t);	
+};
+
+Ray::Ray(){
+
+}
+
+Ray::Ray(nVec _r0,nVec _rd ){
+	r0 = _r0;
+	rd = _rd;
+}
+
+nVec Ray::Point(double t){
+	nVec v = rd.scale(t);
+	return v+r0;
+}
+
+class Sphere
+{
+public:
+	nVec c;
+	int r;
+	double ka;
+	double kd;
+	double ks;
+	double reflection;
+	double refraction;
+	double absorption;
+	double spec_coeff;
+	Vec3b color;
+	int affine;
+	Mat m;
+	// Sphere();
+	Sphere(nVec _c, int _r = 0, Vec3b _color = Vec3b(255,255,255), double _ka = 0.3, double _kd = 0.3, double _ks = 0.3, double _spec_coeff = 2, int _affine =0, Mat _m= Mat(4,4, CV_32FC1, float(0)), double reflection = 0.3, double refraction = 0.3, double absorption = 0.4);
+	pair <double,double> intersect(Ray R);
+	nVec normal(nVec);
+
+};
+
+// Sphere::Sphere(){}
+
+Sphere::Sphere(nVec _c,int _r , Vec3b _color, double _ka, double _kd, double _ks, double _spec_coeff , int _affine, Mat _m, double relection, double refraction, double absorption){
+	c = _c;
+	r = _r;
+	ka = _ka;
+	kd = _kd;
+	ks = _ks;
+	spec_coeff = _spec_coeff;
+	color = _color;
+	affine = _affine;
+	m = _m;
+
+}
+
+pair <double,double> Sphere::intersect(Ray R){
+	double rd_mag = 1;
+	if(affine==1){
+		R.r0 = R.r0.transform(m.inv(),0);
+		R.rd = R.rd.transform(m.inv(),1);
+		rd_mag = R.rd.getMag();
+		R.rd = R.rd.norm();
+	}
+	double A = 1;
+	double B = 2*(R.rd.x*(R.r0.x-c.x) + R.rd.y*(R.r0.y-c.y) + R.rd.z*(R.r0.z-c.z) );
+	double C = (R.r0.x - c.x)*(R.r0.x - c.x) + (R.r0.y - c.y)*(R.r0.y - c.y) + (R.r0.z - c.z)*(R.r0.z - c.z) - r*r;
+
+	double D = B*B -4*A*C; 
+	if(D<0){
+		return make_pair(-1,-1);
+	}
+
+	double t1 = ((-1)*B + sqrt(D))/(2*A*rd_mag);
+	double t2 = ((-1)*B - sqrt(D))/(2*A*rd_mag);
+	return make_pair(t1,t2);
+}
+
+nVec Sphere::normal(nVec point){
+	nVec n = point - c;
+	if(affine==0) return n.norm();
+	else{
+		n = n.transform(m.inv().t(),1);
+		return n.norm();
+	}
+}
+
+class Polygon
+{
+public:
+	int n;
+	vector<nVec> vertices;
+	Vec3b color;
+	double ka;
+	double kd;
+	double ks;
+	double reflection;
+	double refraction;
+	double absorption;
+	double spec_coeff;
+	int affine;
+	Mat m;
+	// Polygon();
+	Polygon(int, vector<nVec> _vertices, Vec3b, double , double, double, double, int,Mat, double, double, double);
+	nVec normal();
+	double intersect(Ray);
+
+};
+
+// Polygon::Polygon(){}
+
+Polygon::Polygon(int _n,vector<nVec> _vertices,Vec3b _color = Vec3b(255,255,255), double _ka = 0.3, double _kd = 0.3, double _ks = 0.3, double _spec_coeff = 2,  int _affine =0, Mat _m= Mat(4,4, CV_32FC1, float(0)), double reflecction = 0.3, double refraction = 0.3, double absorption = 0.4){
+	n = _n;
+	vertices = _vertices;
+	color = _color;
+	ka = _ka;
+	kd = _kd;
+	ks = _ks;
+	spec_coeff = _spec_coeff;
+	affine = _affine;
+	m =_m;
+}
+
+nVec Polygon::normal(){
+	nVec v1 = vertices[1]-vertices[0];
+	nVec v2 = vertices[2]-vertices[1];
+	nVec v3 = v1.crossProd(v2);
+	if(affine==0)	return (v3.norm());
+	else {
+		return v3.transform(m.inv().t(),1).norm();
+	}
+}
+
+double Polygon::intersect(Ray R){
+	if(affine==1){
+		R.r0 = R.r0.transform(m.inv(),0);
+		R.rd = R.rd.transform(m.inv(),1);
+	}
+	nVec n = normal();
+	if(n.dotProd(R.rd)==0){
+		return -1; //parallel case
+	}
+	double D = (-1)*n.dotProd(vertices[0]);
+	double t = (-1)*(n.dotProd(R.r0)+D)/(n.dotProd(R.rd));
+	nVec p = R.Point(t);
+	// cout<<"Point ";
+	// p.print();
+	//check if inside
+	Ray R1;	//ray passing thru intersection point, in the plane
+	R1.r0 = p;
+	R1.rd = (vertices[1]-vertices[0]);	//edge X normal to plane
+	// R1.rd.print();
+	nVec r1norm = R1.rd.crossProd(n); 
+	int nint = 0, n0=0;
+
+	for(int i=0; i< vertices.size(); i++){
+
+		Ray R2;
+		R2.r0 = vertices[i];
+		R2.rd = vertices[(i+1)%(vertices.size())]-vertices[i];
+		// nVec raynorm = n.crossProd(r.rd);
+		// if(R1.rd.dotProd(raynorm)>0){
+		// 	double trls = ((R1.r0-R2.r0).dotProd(raynorm))/ (R1.rd.dotProd(raynorm));
+		// 	nVec pt = R1.point(trls); // intersection pt
+		// 	if( (R2.rd).dotProd(pt - R2.r0) < (R2.rd).dotProd(R2.rd) )  
+		// }
+		// if(R2.rd.dotProd(r1norm)!=0){//changed
+
+			double trls = (-1)*((R2.r0 - p).dotProd(r1norm))/(R2.rd).dotProd(r1norm);
+			nVec p2 = R2.Point(trls);
+			// p2.print();
+			double t2= (p2-R1.r0).dotProd(R1.rd);
+			//changed	
+			// if(p2.x-R1.r0.x!=0){
+
+			// 	t2 = (p2.x-R1.r0.x)/(R1.r0.x-R1.rd.x);///////check for other dir
+			// }
+			// else if (p2.y-R1.r0.y!=0){
+				
+			// 	t2 = (p2.y-R1.r0.y)/(R1.r0.y-R1.rd.y);///////check for other dir
+			// }
+			// else{
+				
+			// 	t2 = (p2.z-R1.r0.z)/(R1.r0.z-R1.rd.z);///////check for other dir
+			// }
+			// R2.Point(trls).print();
+			// cout<<"ind "<<i<<" "<<trls<<" "<<t2<<endl; 
+			if(t2>0){
+
+				if(trls >0.0001 && trls < 0.9999 ){
+					nint++;
+				}
+				if(abs(trls)<0.0001 && abs(trls-1)<0.0001 ){
+					n0++;
+					cout<<"here"<<endl;
+				}
+			}
+		// }
+
+
+
+	}
+	nint+=n0/2;
+	// cout << nint << endl;
+	if(nint % 2 ==1){
+		// cout << t << endl;
+		return t;
+	}
+	// cout << "sadasd" << endl;
+	return -1;
+
+}
+
+
+class Screen{
+public:
+	nVec center;
+	nVec normal;
+	nVec up;
+	nVec right;
+	Screen();
+	Screen(nVec _center, nVec _normal, nVec _up, nVec _right);
+};
+
+Screen::Screen(){}
+
+Screen::Screen(nVec _center, nVec _normal, nVec _up, nVec _right){
+	center = _center;
+	normal = _normal;
+	up = _up;
+	right = _right;
+}
+
+// int main(){
+
+// 	nVec v(0,0,-50);
+// 	// -50,50,0
+// 	nVec v2(-50,50,50);
+// 	Ray r(v,v2);
+// 	// nVec v()
+// 	vector<nVec> nv;
+// 	nVec vv(-50,0,-30);
+// 	nv.push_back(vv);
+// 	nVec vv1(-50,0,30);
+// 	nv.push_back(vv1);
+// 	nVec vv2(-50,100,30);
+// 	nv.push_back(vv2);
+// 	nVec vv3(-50,100,-30);
+// 	nv.push_back(vv3);
+// 	Polygon p(4,nv);
+// 	cout<<p.intersect(r)<<endl;
+// 	// r.Point(p.intersect(r)).print();
+// }
